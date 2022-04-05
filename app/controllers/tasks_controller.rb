bce31f241fdf2b7b9c2e6cacb7ba947519def180
relaxyg <relaxyg4@gmail.com>
2022-04-05 23:07:43 +0600

    add simple style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,23 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: %i[show edit update destroy]
   def index
     @tasks = Task.all
   end
+
   def show; end
+
   def new
     @task = Task.new
   end
+
   def create
-    @task =Task.new(task_params)
+    @task = Task.new(task_params)
     @task.save
     redirect_to task_path(@task)
   end
+
   def edit; end
 
   def update
@@ -24,11 +30,12 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-
   private
+
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
   def set_task
     @task = Task.find(params[:id])
   end
